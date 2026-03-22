74f74adc85e5c9d3aa53ae28604038e2b74339da
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-03-22 17:55:54 +0530

    added contact-support schema

---
+++ Server/Models/contact.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    feedback:{
+        type:String,
+        required:true
+    }
+},{timestamps:true})
+
+const contactModel = mongoose.model("contactSchema",schema);
+export default contactModel;
